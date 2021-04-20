1249a41600960e75291c3564e3717da737d056dd
lillianmei <pinkgirl0201@hotmail.com>
2021-04-20 16:13:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const text = document.querySelector(".text");
+let shadow = '';
+for(var i=0 ; i<30 ; i++){
+    shadow +=(shadow? ',':'')+ -i*1 + 'px ' + i*1 + 'px 0 #d9d9d9';
+}
+text.style.textShadow = shadow;
